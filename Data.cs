a2d168079343a61930888287017a491d9ba40294
Mert Karaca <38283518+sjuhyeon@users.noreply.github.com>
2021-08-25 14:33:04 +0300

    Add files via upload

---
+++ Data.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTPScrape
+{
+    public class Data
+    {
+        public String nameOfAnime { get; set; }
+        public String episodesOfAnime { get; set; }
+        public String bannerOfAnime { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HtmlAgilityPack;
+using System.Threading.Tasks;
+namespace HTTPScrape
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Animes();
+            Console.Read();
+        }
+
+        private static void Animes()
+        {
+
+            var url = "https://9anime.to/home";
+            HtmlWeb web = new HtmlWeb();
+            var doc = web.Load(url);
+            var nodes = doc.DocumentNode.SelectNodes("/html/body/div[1]/div/aside[1]/section[2]/div[2]/ul/li[position()>1]"); // xPath of 'a'
+
+
+            foreach (var item in nodes)
+            {
+                Console.WriteLine("Name:{0}\nEpisode:{1}\n",item.SelectSingleNode("a[2]").InnerText,item.SelectSingleNode("a[1]").InnerText);
+            }
+        }
+    }
+}
